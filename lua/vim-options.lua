662dfddffde214b51320ad90749fa4c3247ed89e
Markas Korotkovas <Markas.Korotkovas@efirstbank.com>
2024-03-04 18:02:16 -0700

    first

---
+++ lua/vim-options.lua
@@ -0,0 +1,11 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+--set leader key to be space
+vim.g.mapleader=" "
+
+--show line numbers
+vim.wo.number = true
+
