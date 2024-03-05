dc09a41fd86fffbbb03eb5861cca8fac0ac88583
Markas Korotkovas <Markas.Korotkovas@efirstbank.com>
2024-03-05 16:35:36 -0700

    removed the neo-tree and set up the nvim-tree for performance reasons on win

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,6 +6,14 @@ vim.cmd("set shiftwidth=2")
 --set leader key to be space
 vim.g.mapleader=" "
 
+--better navigation between panes
+vim.keymap.set('n', '<c-Up>',    ':wincmd k<CR>')
+vim.keymap.set('n', '<c-Down>',  ':wincmd j<CR>')
+vim.keymap.set('n', '<c-Left>',  ':wincmd h<CR>')
+vim.keymap.set('n', '<c-Right>', ':wincmd l<CR>')
+vim.keymap.set('n', '<c-q>',     ':q<CR>')
+vim.keymap.set('n', '<c-s>',     ':w<CR>')
+
 --show line numbers
 vim.wo.number = true
 
