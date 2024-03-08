ae4280c71a9b948c98046a9f96e4c72a89f89799
Markas Korotkovas <Markas.Korotkovas@efirstbank.com>
2024-03-08 12:05:20 -0700

    tweaked mason

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,9 +9,11 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
-    opts = {
-      auto_install = true,
-    },
+    config = function()
+      require("mason-lspconfig").setup({
+        automatic_installation = true
+      })
+    end
   },
   {
     "neovim/nvim-lspconfig",
