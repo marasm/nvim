27dffe2bf9e1cd4ac832a54092a47246c7760e48
Markas Korotkovas <mkorotkovas@yahoo.com>
2024-03-27 00:13:12 -0600

    added the recoring mode (from noice) to the lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,12 +1,12 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "auto", --will match whatever is set for nvim colorscheme
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "auto", --will match whatever is set for nvim colorscheme
         icons_enabled = true,
-        component_separators = { left = '|', right = '|'},
-        section_separators = { left = ' ', right = ' '},
+        component_separators = { left = "|", right = "|" },
+        section_separators = { left = " ", right = " " },
         disabled_filetypes = {
           statusline = {},
           winbar = {},
@@ -18,8 +18,20 @@ return {
           statusline = 1000,
           tabline = 1000,
           winbar = 1000,
-        }
-		  },
-		})
-	end,
+        },
+      },
+      sections = {
+        lualine_x = {
+          {
+            require("noice").api.statusline.mode.get,
+            cond = require("noice").api.statusline.mode.has,
+            color = { fg = "#ff9e64" },
+          },
+          "encoding",
+          "fileformat",
+          "filetype",
+        },
+      },
+    })
+  end,
 }
